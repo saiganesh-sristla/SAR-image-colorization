b8b43711803594e3c5296b3aed7bbc668ca0e7b4
saiganesh-sristla <saiganesh3108@gmail.com>
2024-09-05 20:06:34 +0530

    Initialize frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="font-bold text-2xl m-2 p-2 text-center">
+        SAR IMAGE COLORIZATION
+      </div>
+    </>
+  );
+}
+
+export default App;
