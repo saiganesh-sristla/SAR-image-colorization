0c0492eaae0c32a41c3247a44b9ad4574ff15ac5
saiganesh-sristla <saiganesh3108@gmail.com>
2024-09-07 13:49:06 +0530

    init

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,37 @@
 import ImageCard from "./components/ImageCard";
-import r from "../assets/random.png";
-import ImageUpload from "./components/ImageUpload";
-import Navbar from "./components/Navbar";
+import { useState } from "react";
 
 function App() {
+  const [image, setImage] = useState("");
+  const [ColorizedImage, setColorizedImage] = useState("");
+
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    setImage(URL.createObjectURL(file));
+  }
+
+  const handleImageUpload = () => {
+    //api call 
+
+    setColorizedImage("");
+  }
+
   return (
-    <div className="bg-slate-400 h-screen">
-      <Navbar/>
-      <ImageUpload/>
-      <div className="flex justify-center gap-10 mt-20 px-10">
-        <ImageCard src={r} alt={"original image"} title={"Original Image"}/>
-        <ImageCard src={r} alt={"Colorized image"} title={"Colorized Image"}/>
+    <div className="bg-black h-full text-white bg-gradient-to-b from-black to-gray-900">
+      <div className="text-2xl p-4 font-bold text-blue-500 shadow-2xl bg-black">SAR IMAGE COLORIZARION</div>
+      <div className="flex flex-col items-center gap-2 p-20">
+      <label className="font-semibold text-xl" for="file">
+        Select Image
+      </label>
+      <input className="pl-20" type="file" id="file" onChange={handleImageChange}></input>
+      <button className="inline-flex items-center gap-2 rounded-lg bg-indigo-600 px-4 py-2 text-white hover:bg-indigo-700" onClick={handleImageUpload}>
+        upload
+      </button>
+    </div>
+      <div className="flex justify-center gap-10  px-10">
+        <ImageCard src={image} alt={"original image"} title={"Original Image"}/>
+        <ImageCard src={ColorizedImage} alt={"Colorized image"} title={"Colorized Image"}/>
       </div>
     </div>
   );
--- Frontend/src/components/ImageCard.jsx
+++ Frontend/src/components/ImageCard.jsx
@@ -1,9 +1,19 @@
 const ImageCard = (props) => {
 
+  if(props.src == ""){
+    return (
+      <div className="bg-slate-800 shadow-lg w-96 h-96">
+        <div className="text-center p-2">
+          <img className="w-96 h-80 rounded-lg" src={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJQAAACUCAMAAABC4vDmAAAAYFBMVEXw8fP6+/3z9PaAi5F4hIp+h4/BxMjCx81vd35oeXzQ1dbT1Nft8fTu8fCLlZzz9PTa3t/j5+h2foSnr7SGkJXq6+6YnqSYoqWdpqu0urt4g4Xe4eXS195rd3za2t3Iy847O4M1AAAB3klEQVR4nO3Y226CQBSF4XHPjG3VykGgKirv/5YdBJRTdZOYPZiu/xIM+bIChKgWM0z5BowFFDeguAHFDShuQHEDihtQ3N4EReJxUEo4oIACCiiggAIKKKCAAgoooIACCiiggAIKKKD6BZP++5VBpcVyQkX6LYCizFh+2mQLAdTmvJ5w29H6cyOB+jrNEbWa8oAC9U6oMEy7Z/2jop841t2H0zuK9kZrqzsK7yjlTFqbzlTeUZRcUWH7mH/UIXamJJgVSqnDdruPOkdmgKI0Jf+vBKInXyYeUBTmwYPfekHR6qjzx1uJoyjU1m6jka3uF5FGuZ1s+WIabEX5zheq3Kk0DbaiKIl3zWVkUeX9VGfzoLUV5drouNlKFNXsNNjK7VSeMPVWkig6GX3P3LeiPKmO1VtJokJrWyi3Vf2+qnfSt63EUG4n3cvYqNzqtlOlEl2qt9Ntq9ZOzVYrGdT5MtjpqrJ5Z6fqviIp1FIPdqq2OiX9E2Z3kUEdzRjpShg5lMUyqL9M4wE1K1QQzxBFmZlU9vSCL0CptPiYUJGKoNxlAnbqySf861CvDSiggAIKKKCAAgoooIACCiiggAIKKKCAAurfocRjoGYQUNyA4gYUN6C4AcUNKG5AcQOK2y+c0USun3RQnwAAAABJRU5ErkJggg=="} alt={props.alt}></img>
+          <span className="font-medium text-xl p-2 text-white ">{props.title}</span>
+        </div>
+      </div>
+    );
+  }
   return (
-    <div className="bg-slate-800 shadow-lg w-72">
+    <div className="bg-slate-800 shadow-lg w-96 rounded-xl">
       <div className="text-center p-2">
-        <img className="w-full rounded-lg" src={props.src} alt={props.alt}></img>
+        <img className="w-96 h-80 rounded-lg" src={props.src} alt={props.alt}></img>
         <span className="font-medium text-xl p-2 text-white ">{props.title}</span>
       </div>
     </div>
